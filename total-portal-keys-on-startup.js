b2cb57351cc9accff279e0913687cb4b0887f74c
Smooklu <37220586+Smooklu@users.noreply.github.com>
2021-05-03 16:01:11 -0500

    Create total-portal-keys-on-startup.js

---
+++ total-portal-keys-on-startup.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         Total Portal Keys on Startup
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Displays total portal keys on startup.
+// @author       Smooklu
+// @match        https://intel.ingress.com/
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    setTimeout(function(){
+    document.getElementById('inv_button').click()
+}, 3000);
+    setTimeout(function(){
+    var pkeys = Array.from(document.querySelectorAll("br ~ div.inv_container"));pkeys.shift()
+        window.totalkeyamount = 0
+        function getKeyAmount(item) {
+            var keyamount = item.children[1].innerText.replace('x','')
+            window.totalkeyamount = window.totalkeyamount + parseInt(keyamount)
+        }
+        pkeys.forEach(getKeyAmount)
+        alert(window.totalkeyamount)
+}, 5000);
+
+})();
